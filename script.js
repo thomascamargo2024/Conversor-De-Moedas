20e306f81fd5a528c0c05f901232a00965c79113
thomascamargo2024 <thomacamargo@gmail.com>
2026-03-30 18:15:01 -0300

    Conversor de moedas usando Html, Css e JavaScript

---
+++ script.js
@@ -0,0 +1,55 @@
+let botaoConverter = document.querySelector('.botao-converte')
+let valorReal = document.querySelector('.valorReal')
+let valorDolar = document.querySelector('.valorDolar')
+let caixaSelect = document.querySelector('#caixaSelect')
+
+const valorDolarAtual = 5.25
+const valorEuroAtual = 6.03
+
+
+    let converterValores = () => {
+    let valorParaConverter = Number(document.querySelector('#Valor-Converter').value)
+    
+    if (!valorParaConverter) return
+
+    let valorDolarConvertido = valorParaConverter / valorDolarAtual
+    let valorEuroConvertido = valorParaConverter / valorEuroAtual
+
+    valorReal.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    }).format(valorParaConverter)
+
+    if (caixaSelect.value == "US$ Dólar") {
+        valorDolar.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD",
+        }).format(valorDolarConvertido)
+    }
+
+    if (caixaSelect.value == "€ Euro") {
+        valorDolar.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR",
+        }).format(valorEuroConvertido)
+    }
+}
+
+botaoConverter.addEventListener('click', converterValores)
+
+caixaSelect.addEventListener("change", () => {
+    const textoEuro = document.querySelector('.texto-Dolar')
+    const imgDolar = document.querySelector('#img-Dolar')
+
+    if (caixaSelect.value == "€ Euro") {
+        textoEuro.innerHTML = "€ Euro"
+        imgDolar.src = "assets/icons8-euro-48.png"
+    }
+    else if (caixaSelect.value == "US$ Dólar") {
+        textoEuro.innerHTML = "Dólar Americano"
+        imgDolar.src = "assets/icons8-circular-dos-eua-48.png"
+    }
+    converterValores()
+})
+
+
